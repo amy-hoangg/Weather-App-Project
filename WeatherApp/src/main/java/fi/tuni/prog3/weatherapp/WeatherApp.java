202c54fd78999381247fc1360f2cc6069d00f11b
Abdurrahman Faig <abdurrahman.faig@tuni.fi>
2023-11-26 18:06:55 +0200

    Added json parsing method and created weatherdata class, also Successful API connection established, data retrieving possible

--- WeatherApp/src/main/java/fi/tuni/prog3/weatherapp/WeatherApp.java
+++ WeatherApp/src/main/java/fi/tuni/prog3/weatherapp/WeatherApp.java
@@ -23,7 +23,6 @@ import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
-<<<<<<< HEAD
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -37,10 +36,8 @@ import java.util.Map;
 import com.google.gson.Gson;
 
 // This version is being maintained by Abu
+// Latest update: API call is working
 
-=======
-//Change made by Abu
->>>>>>> 02ea708a7f487d18581dfefe900a5b2e49ccd93c
 /**
  * JavaFX Sisu
  */
@@ -221,13 +218,7 @@ public class WeatherApp extends Application {
     }
 
     private HBox getBottomHBox() {
-<<<<<<< HEAD
         // Creating a VBox for the right side.
-=======
-
-        // Test comment for test commit
-        //Creating a VBox for the right side.
->>>>>>> 02ea708a7f487d18581dfefe900a5b2e49ccd93c
         HBox rightHBox = new HBox();
         rightHBox.setPrefHeight(330);
         rightHBox.setStyle("-fx-background-color: #b1c2d4;");

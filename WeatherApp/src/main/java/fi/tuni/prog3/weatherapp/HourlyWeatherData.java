65a4d00b682ad22d7b87408636a6369fce88346b
Hans Ravna-Pieski <hans.ravna-pieski@tuni.fi>
2023-12-07 15:48:26 +0200

    Stripped unnecessary data from HourlyWeatherData.java

--- WeatherApp/src/main/java/fi/tuni/prog3/weatherapp/HourlyWeatherData.java
+++ WeatherApp/src/main/java/fi/tuni/prog3/weatherapp/HourlyWeatherData.java
@@ -3,35 +3,8 @@ package fi.tuni.prog3.weatherapp;
 import java.util.List;
 
 public class HourlyWeatherData {
-    public String cod;
-    public int message;
-    public int cnt;
     public List<WeatherData> list;
     public City city;
-
-    public String getCod() {
-        return cod;
-    }
-
-    public void setCod(String cod) {
-        this.cod = cod;
-    }
-
-    public int getMessage() {
-        return message;
-    }
-
-    public void setMessage(int message) {
-        this.message = message;
-    }
-
-    public int getCnt() {
-        return cnt;
-    }
-
-    public void setCnt(int cnt) {
-        this.cnt = cnt;
-    }
 
     public List<WeatherData> getList() {
         return list;
@@ -50,22 +23,17 @@ public class HourlyWeatherData {
     }
 
     public static class WeatherData {
-        public long dt;
+        public String dt_txt;
         public Main main;
         public List<Weather> weather;
-        public Clouds clouds;
         public Wind wind;
-        public int visibility;
-        public double pop;
-        public Sys sys;
-        public String dt_txt;
 
-        public long getDt() {
-            return dt;
+        public String getDt_txt() {
+            return dt_txt;
         }
 
-        public void setDt(long dt) {
-            this.dt = dt;
+        public void setDt_txt(String dt_txt) {
+            this.dt_txt = dt_txt;
         }
 
         public Main getMain() {
@@ -84,14 +52,6 @@ public class HourlyWeatherData {
             this.weather = weather;
         }
 
-        public Clouds getClouds() {
-            return clouds;
-        }
-
-        public void setClouds(Clouds clouds) {
-            this.clouds = clouds;
-        }
-
         public Wind getWind() {
             return wind;
         }
@@ -99,50 +59,11 @@ public class HourlyWeatherData {
         public void setWind(Wind wind) {
             this.wind = wind;
         }
-
-        public int getVisibility() {
-            return visibility;
-        }
-
-        public void setVisibility(int visibility) {
-            this.visibility = visibility;
-        }
-
-        public double getPop() {
-            return pop;
-        }
-
-        public void setPop(double pop) {
-            this.pop = pop;
-        }
-
-        public Sys getSys() {
-            return sys;
-        }
-
-        public void setSys(Sys sys) {
-            this.sys = sys;
-        }
-
-        public String getDt_txt() {
-            return dt_txt;
-        }
-
-        public void setDt_txt(String dt_txt) {
-            this.dt_txt = dt_txt;
-        }
     }
 
     public static class Main {
         public double temp;
-        public double feels_like;
-        public double temp_min;
-        public double temp_max;
-        public int pressure;
-        public int sea_level;
-        public int grnd_level;
         public int humidity;
-        public double temp_kf;
 
         public double getTemp() {
             return temp;
@@ -152,54 +73,6 @@ public class HourlyWeatherData {
             this.temp = temp;
         }
 
-        public double getFeels_like() {
-            return feels_like;
-        }
-
-        public void setFeels_like(double feels_like) {
-            this.feels_like = feels_like;
-        }
-
-        public double getTemp_min() {
-            return temp_min;
-        }
-
-        public void setTemp_min(double temp_min) {
-            this.temp_min = temp_min;
-        }
-
-        public double getTemp_max() {
-            return temp_max;
-        }
-
-        public void setTemp_max(double temp_max) {
-            this.temp_max = temp_max;
-        }
-
-        public int getPressure() {
-            return pressure;
-        }
-
-        public void setPressure(int pressure) {
-            this.pressure = pressure;
-        }
-
-        public int getSea_level() {
-            return sea_level;
-        }
-
-        public void setSea_level(int sea_level) {
-            this.sea_level = sea_level;
-        }
-
-        public int getGrnd_level() {
-            return grnd_level;
-        }
-
-        public void setGrnd_level(int grnd_level) {
-            this.grnd_level = grnd_level;
-        }
-
         public int getHumidity() {
             return humidity;
         }
@@ -207,45 +80,10 @@ public class HourlyWeatherData {
         public void setHumidity(int humidity) {
             this.humidity = humidity;
         }
-
-        public double getTemp_kf() {
-            return temp_kf;
-        }
-
-        public void setTemp_kf(double temp_kf) {
-            this.temp_kf = temp_kf;
-        }
     }
 
     public static class Weather {
-        public int id;
-        public String main;
-        public String description;
         public String icon;
-
-        public int getId() {
-            return id;
-        }
-
-        public void setId(int id) {
-            this.id = id;
-        }
-
-        public String getMain() {
-            return main;
-        }
-
-        public void setMain(String main) {
-            this.main = main;
-        }
-
-        public String getDescription() {
-            return description;
-        }
-
-        public void setDescription(String description) {
-            this.description = description;
-        }
 
         public String getIcon() {
             return icon;
@@ -256,22 +94,8 @@ public class HourlyWeatherData {
         }
     }
 
-    public static class Clouds {
-        public int all;
-
-        public int getAll() {
-            return all;
-        }
-
-        public void setAll(int all) {
-            this.all = all;
-        }
-    }
-
     public static class Wind {
         public double speed;
-        public int deg;
-        public double gust;
 
         public double getSpeed() {
             return speed;
@@ -280,53 +104,10 @@ public class HourlyWeatherData {
         public void setSpeed(double speed) {
             this.speed = speed;
         }
-
-        public int getDeg() {
-            return deg;
-        }
-
-        public void setDeg(int deg) {
-            this.deg = deg;
-        }
-
-        public double getGust() {
-            return gust;
-        }
-
-        public void setGust(double gust) {
-            this.gust = gust;
-        }
-    }
-
-    public static class Sys {
-        public String pod;
-
-        public String getPod() {
-            return pod;
-        }
-
-        public void setPod(String pod) {
-            this.pod = pod;
-        }
     }
 
     public static class City {
-        public int id;
         public String name;
-        public Coord coord;
-        public String country;
-        public int population;
-        public int timezone;
-        public long sunrise;
-        public long sunset;
-
-        public int getId() {
-            return id;
-        }
-
-        public void setId(int id) {
-            this.id = id;
-        }
 
         public String getName() {
             return name;
@@ -334,75 +115,6 @@ public class HourlyWeatherData {
 
         public void setName(String name) {
             this.name = name;
-        }
-
-        public Coord getCoord() {
-            return coord;
-        }
-
-        public void setCoord(Coord coord) {
-            this.coord = coord;
-        }
-
-        public String getCountry() {
-            return country;
-        }
-
-        public void setCountry(String country) {
-            this.country = country;
-        }
-
-        public int getPopulation() {
-            return population;
-        }
-
-        public void setPopulation(int population) {
-            this.population = population;
-        }
-
-        public int getTimezone() {
-            return timezone;
-        }
-
-        public void setTimezone(int timezone) {
-            this.timezone = timezone;
-        }
-
-        public long getSunrise() {
-            return sunrise;
-        }
-
-        public void setSunrise(long sunrise) {
-            this.sunrise = sunrise;
-        }
-
-        public long getSunset() {
-            return sunset;
-        }
-
-        public void setSunset(long sunset) {
-            this.sunset = sunset;
-        }
-    }
-
-    public static class Coord {
-        public double lat;
-        public double lon;
-
-        public double getLat() {
-            return lat;
-        }
-
-        public void setLat(double lat) {
-            this.lat = lat;
-        }
-
-        public double getLon() {
-            return lon;
-        }
-
-        public void setLon(double lon) {
-            this.lon = lon;
         }
     }
 }
